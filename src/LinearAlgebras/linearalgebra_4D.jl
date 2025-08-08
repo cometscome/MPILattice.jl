35aaf738a3a8a43ceeded94308193bb034cd69f1
cometscome <cometscome@gmail.com>
2025-08-08 22:47:53 +0200

    mul

--- src/LinearAlgebras/linearalgebra_4D.jl
+++ src/LinearAlgebras/linearalgebra_4D.jl
@@ -20,14 +20,14 @@ function get_4Dindex(i, dims)
     return ix, iy, iz, it
 end
 
-struct Mulkernel{NC1,NC2,NC3}
+struct ulkernel{NC1,NC2,NC3}
 end
 
 function LinearAlgebra.mul!(C::LatticeMatrix{4,T1,AT1,NC1,NC2},
     A::LatticeMatrix{4,T2,AT2,NC1,NC3}, B::LatticeMatrix{4,T3,AT3,NC3,NC2}) where {T1,T2,T3,AT1,AT2,AT3,NC1,NC2,NC3}
-    mulkernel = Mulkernel{NC1,NC2,NC3}()
+
     JACC.parallel_for(
-        prod(C.PN), kernel_4Dmatrix_mul!, C.A, A.A, B.A, mulkernel, C.nw, C.dims
+        prod(C.PN), kernel_4Dmatrix_mul!, C.A, A.A, B.A, NC1, NC2, NC3, C.nw, C.dims
     )
     set_halo!(C)
 end
@@ -39,7 +39,7 @@ function kernel_4Dvector_mul!(i, C, A, B, NC, nw, dims)
     end
 end
 
-function kernel_4Dmatrix_mul!(i, C, A, B, mulkernel::Mulkernel{NC1,NC2,NC3}, nw, dims) where {NC1,NC2,NC3}
+function kernel_4Dmatrix_mul!(i, C, A, B, NC1, NC2, NC3, nw, dims)
     indices = get_4Dindex(i, dims)
     @inbounds for ic = 1:NC1
         for jc = 1:NC2
